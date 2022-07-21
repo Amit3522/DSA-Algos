8f5290a80123eaf34c2cdb770d6a85f328b934ac
Amit3522 <109713793+Amit3522@users.noreply.github.com>
2022-07-21 10:22:50 +0530

    Add files via upload

---
+++ Pre Intermediate/Read print ints.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // importing predrfined code
+
+class Main {
+	public static void main(String args[]) {
+		// Build a scanner
+		Scanner AmitScanner = new Scanner(System.in);
+
+		//create a bucket to store numbers
+		int N= AmitScanner.nextInt(); // creating a bucket to hold integers
+		
+
+
+		System.out.println("Making " + N + " packs of Maggi");
+		
+
+	}
+}
---
+++ Pre Intermediate/Read print strings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // importing predrfined code
+
+class Main {
+	public static void main(String args[]) {
+	
+	//Read a name
+
+	//Build a Scanner
+	Scanner akScanner = new Scanner(System.in); 
+	//create a bukcet for string and read custom input using scanner 
+	String Name = akScanner.nextLine();
+	//print o/p
+	System.out.println(Name);
+
+
+	}
+}
